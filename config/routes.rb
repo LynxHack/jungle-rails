6b0854117941bd3ca448f27051e30ea15bd75da0
LynxHack <KevinLynx_L@hotmail.com>
2018-07-20 02:41:33 +0000

    Added nav bar for login and registration, added views and routes for loging and signup

--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,19 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:edit, :show, :update]#, :destroy]
   end
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
