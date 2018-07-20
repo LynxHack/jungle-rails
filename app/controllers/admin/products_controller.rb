32d49adfbd5a6ccea11df32ea7eaa90ca3157902
LynxHack <KevinLynx_L@hotmail.com>
2018-07-20 06:04:03 +0000

    Added http auth for admin access

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ProductsController < ApplicationController
+  ##force auth during admin access
+  http_basic_authenticate_with name: ENV['AUTH_USERNAME'].to_s, password: ENV['AUTH_PASSWORD'].to_s
 
   def index
     @products = Product.order(id: :desc).all
